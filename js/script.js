d0e38821329f0a5efc18c2781ffefa84faaeeb85
alexlikenew <alexlikenew@gmail.com>
2024-12-31 19:18:38 +0100

    Desktop page completed

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const mobileMenuBtnOpen = document.querySelector('.nav__mobile__open')
 const mobileMenuBtnClose = document.querySelector('.nav__mobile__popup__close')
 const mobileMenuPopup = document.querySelector('.nav__mobile__popup')
 // FACEBOOK POPUP
-const facebookAdPopupCloseArrow = document.querySelector('.header__facebook-banner__like__text__close-icon')
-const facebookAdPopup = document.querySelector('.header__facebook-banner')
+// const facebookAdPopupCloseArrow = document.querySelector('.header__facebook-banner__like__text__close-icon')
+// const facebookAdPopup = document.querySelector('.header__facebook-banner')
 // SECTION KNOW MORE HINTS
 const knowMoreHints = document.querySelectorAll('.know-more__text__table__element')
 const knowMoreHintsArrow = document.querySelectorAll('.know-more__text__table__element__arrow')
@@ -28,10 +28,10 @@ mobileMenuBtnClose.addEventListener('click', mobileMenuClose)
 //============= MOBILE MENU OPEN & CLOSE ====================
 
 //============= SECTION FACEBOOK POPUP CLOSE ====================
-const facebookAdPopupCloseFunc = () => {
-	facebookAdPopup.classList.add('hide')
-}
-facebookAdPopupCloseArrow.addEventListener('click', facebookAdPopupCloseFunc)
+// const facebookAdPopupCloseFunc = () => {
+// 	facebookAdPopup.classList.add('hide')
+// }
+// facebookAdPopupCloseArrow.addEventListener('click', facebookAdPopupCloseFunc)
 //============= SECTION FASCEBOOK POPUP CLOSE ====================
 
 //============= SECTION KNOW MORE HINTS OPEN & CLOSE ====================
