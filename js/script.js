811cae266bb9772b4103540e147c3177da311796
alexlikenew <alexlikenew@gmail.com>
2024-12-31 13:30:53 +0100

    Main animations completed

--- js/script.js
+++ js/script.js
@@ -1,27 +1,62 @@
+//====================== CONSTS ===============================
+// MOBILE MENU
 const mobileMenuBtnOpen = document.querySelector('.nav__mobile__open')
 const mobileMenuBtnClose = document.querySelector('.nav__mobile__popup__close')
 const mobileMenuPopup = document.querySelector('.nav__mobile__popup')
+// FACEBOOK POPUP
+const facebookAdPopupCloseArrow = document.querySelector('.header__facebook-banner__like__text__close-icon')
+const facebookAdPopup = document.querySelector('.header__facebook-banner')
+// SECTION KNOW MORE HINTS
 const knowMoreHints = document.querySelectorAll('.know-more__text__table__element')
+const knowMoreHintsArrow = document.querySelectorAll('.know-more__text__table__element__arrow')
+// SECTION FAQ HINTS
+const faqHints = document.querySelectorAll('.faq__table__element')
+const faqhintsPlus = document.querySelectorAll('.faq__table__element__plus')
 
+//====================== CONSTS ===============================
+
+//====================== FUNCTIONS ===============================
 //============= MOBILE MENU OPEN & CLOSE ====================
 const mobileMenuOpen = () => {
-	mobileMenuPopup.classList.add('show')
+	mobileMenuPopup.classList.add('showMenuPopup')
 }
 const mobileMenuClose = () => {
-	mobileMenuPopup.classList.remove('show')
+	mobileMenuPopup.classList.remove('showMenuPopup')
 }
 mobileMenuBtnOpen.addEventListener('click', mobileMenuOpen)
 mobileMenuBtnClose.addEventListener('click', mobileMenuClose)
 //============= MOBILE MENU OPEN & CLOSE ====================
 
-//============= SECTION KNOW MORE HINTS OPEN & CLOSE ====================
-const knowMoreOpenHint = element => {
-	console.log(element.children[1].classList.add('show'))
+//============= SECTION FACEBOOK POPUP CLOSE ====================
+const facebookAdPopupCloseFunc = () => {
+	facebookAdPopup.classList.add('hide')
 }
+facebookAdPopupCloseArrow.addEventListener('click', facebookAdPopupCloseFunc)
+
+//============= SECTION FASCEBOOK POPUP CLOSE ====================
+
+//============= SECTION KNOW MORE HINTS OPEN & CLOSE ====================
+
 knowMoreHints.forEach(element => {
-	element.addEventListener('click', knowMoreOpenHint(element))
+	const knowMoreOpenHint = () => {
+		element.children[1].classList.toggle('show')
+		element.children[0].children[1].classList.toggle('spin90')
+	}
+	element.addEventListener('click', knowMoreOpenHint)
 })
+//============= SECTION KNOW MORE HINTS OPEN & CLOSE ====================
+//============= SECTION FAQ HINTS OPEN & CLOSE ====================
+
+faqHints.forEach(element => {
+	const faqOpenHint = () => {
+		element.children[1].classList.toggle('show')
+		element.children[0].children[1].classList.toggle('spin45')
+	}
+	element.addEventListener('click', faqOpenHint)
+})
+//============= SECTION FAQ HINTS OPEN & CLOSE ====================
 
 // const arrowButtons = document.querySelectorAll('.know-more__text__table__element__arrow')
 
 // arrowButtons.forEach(element => element.addEventListener(element.classList.toggle('rotateArrow ')))
+//====================== FUNCTIONS ===============================
